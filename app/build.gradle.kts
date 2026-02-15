f9a494229534788d65b84a69f077d89c2d9133ed
Stéphane <foxace66@gmail.com>
2026-02-15 14:57:02 +0100

    fix: add debug signing config to release build type
    Allows installing the release APK without a dedicated keystore.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ android {
 
         release {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("debug")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro",
